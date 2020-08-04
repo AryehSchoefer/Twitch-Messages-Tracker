75d84dd4f41c0935abe519bc95a1c97a7a04c7f8
AryehSchoefer <aryeh.shmuel@googlemail.com>
2020-08-04 07:37:25 +0200

    lazy readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Twitch Messages Tracker
+
+Web application that tracks chat messages of a specified user in a specified channel. Powered by [tmi.js](https://tmijs.com/ 'tmi.js homepage').
--- client/app.js
+++ client/app.js
@@ -12,104 +12,106 @@ const userMemory = []
 
 let currentlyTracking = false
 form.addEventListener('submit', async (event) => {
-    event.preventDefault()
-    userMemory.length = 0
-    serverError.style.display = 'none'
+	event.preventDefault()
+	userMemory.length = 0
+	serverError.style.display = 'none'
 
-    if (!currentlyTracking) { // start tracking button
-        startButton.style.display = 'none'
-        stopButton.style.display = 'inline-block'
+	if (!currentlyTracking) {
+		// start tracking button
+		startButton.style.display = 'none'
+		stopButton.style.display = 'inline-block'
 
-        const userInput = {
-            channelname: channelnameInput.value,
-            username: usernameInput.value
-        }
+		const userInput = {
+			channelname: channelnameInput.value,
+			username: usernameInput.value,
+		}
 
-        currentlyTracking = true
+		currentlyTracking = true
 
-        userMemory.push(userInput)
-        await sendUserinput(userInput)
-        getMessages()
-    } else { // stop tracking button
-        startButton.style.display = 'inline-block'
-        stopButton.style.display = 'none'
-        currentlyTracking = false
-    }
+		userMemory.push(userInput)
+		await sendUserinput(userInput)
+		getMessages()
+	} else {
+		// stop tracking button
+		startButton.style.display = 'inline-block'
+		stopButton.style.display = 'none'
+		currentlyTracking = false
+	}
 })
 
 async function sendUserinput(userInput) {
-    const { channelname } = userInput
-    const { username } = userInput
+	const { channelname } = userInput
+	const { username } = userInput
 
-    try {
-        const response = await fetch(`${apiBase}/insertMsg`, {
-            method: 'Post',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify({
-                channelname: channelname,
-                username: username
-            })
-        })
-        const chatMessages = await response.json()
-        return chatMessages
-    } catch (err) {
-        serverError.style.display = 'block'
-        startButton.style.display = 'inline-block'
-        stopButton.style.display = 'none'
-        return err
-    }
+	try {
+		const response = await fetch(`${apiBase}/insertMsg`, {
+			method: 'Post',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+			body: JSON.stringify({
+				channelname: channelname,
+				username: username,
+			}),
+		})
+		const chatMessages = await response.json()
+		return chatMessages
+	} catch (err) {
+		serverError.style.display = 'block'
+		startButton.style.display = 'inline-block'
+		stopButton.style.display = 'none'
+		return err
+	}
 }
 
 async function getMessages() {
-    const { channelname } = userMemory[0]
-    const { username } = userMemory[0]
-    console.log(`${channelname}, ${username}`)
+	const { channelname } = userMemory[0]
+	const { username } = userMemory[0]
+	console.log(`${channelname}, ${username}`)
 
-    const response = await fetch(`${apiBase}/messages`, {
-        method: 'Post',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        body: JSON.stringify({
-            channelname: channelname,
-            username: username
-        })
-    })
-    const messages = await response.json()
-    while (messagesSection.firstChild) {
-        messagesSection.removeChild(messagesSection.firstChild)
-    }
-    messages.reverse()
-    messages.forEach(message => {
-        const messageBox = document.createElement('div')
-        messageBox.setAttribute('class', 'message')
+	const response = await fetch(`${apiBase}/messages`, {
+		method: 'Post',
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: 'application/json',
+		},
+		body: JSON.stringify({
+			channelname: channelname,
+			username: username,
+		}),
+	})
+	const messages = await response.json()
+	while (messagesSection.firstChild) {
+		messagesSection.removeChild(messagesSection.firstChild)
+	}
+	messages.reverse()
+	messages.forEach((message) => {
+		const messageBox = document.createElement('div')
+		messageBox.setAttribute('class', 'message')
 
-        const messageInfo = document.createElement('h3')
-        messageInfo.setAttribute('class', 'messageInfo')
-        messageInfo.textContent = `${message.username} in ${message.channelname}'s chat`
+		const messageInfo = document.createElement('h3')
+		messageInfo.setAttribute('class', 'messageInfo')
+		messageInfo.textContent = `${message.username} in ${message.channelname}'s chat`
 
-        const chatMessage = document.createElement('p')
-        chatMessage.setAttribute('class', 'chatmessage')
-        chatMessage.textContent = message.message
+		const chatMessage = document.createElement('p')
+		chatMessage.setAttribute('class', 'chatmessage')
+		chatMessage.textContent = message.message
 
-        const dateInfo = document.createElement('p')
-        dateInfo.setAttribute('class', 'date')
-        const date = new Date(message.date)
-        dateInfo.textContent = date.toUTCString()
+		const dateInfo = document.createElement('p')
+		dateInfo.setAttribute('class', 'date')
+		const date = new Date(message.date)
+		dateInfo.textContent = date.toUTCString()
 
-        messageBox.appendChild(messageInfo)
-        messageBox.appendChild(chatMessage)
-        messageBox.appendChild(dateInfo)
+		messageBox.appendChild(messageInfo)
+		messageBox.appendChild(chatMessage)
+		messageBox.appendChild(dateInfo)
 
-        messagesSection.appendChild(messageBox)
-    })
+		messagesSection.appendChild(messageBox)
+	})
 
-    console.log(messages)
-    if (currentlyTracking) {
-        setTimeout(getMessages, 10000)
-    }
+	console.log(messages)
+	if (currentlyTracking) {
+		setTimeout(getMessages, 10000)
+	}
 }
